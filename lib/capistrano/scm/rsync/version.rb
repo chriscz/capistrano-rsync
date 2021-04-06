de634523f6c5672e58cc0cdac5014cc6e818aaad
Chris Coetzee <chriscz93@gmail.com>
2021-04-06 08:13:45 +0200

    Fix access to version

--- lib/capistrano/scm/rsync/version.rb
+++ lib/capistrano/scm/rsync/version.rb
@@ -1,9 +1,5 @@
 require "capistrano/scm/plugin"
 
-module Capistrano
-  module SCM
-    class Rsync < Plugin
-      VERSION = "1.4.2"
-    end
-  end
+class Capistrano::SCM::Rsync < Plugin
+  VERSION = "1.4.2"
 end
